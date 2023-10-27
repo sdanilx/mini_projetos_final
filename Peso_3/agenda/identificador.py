962e01db106fac13311dea7bb3b28943b3bb11d6
RianRs <rian.ssrb@gmail.com>
2023-10-26 23:45:30 -0300

    in

---
+++ Peso_3/agenda/identificador.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Identificador(Enum):
+    CELULAR = ("Celular")
+    CASA = ("Casa")
+    TRABALHO = ("Trabalho")
+
+    def __init__(self, nome:str):
+        self.identificador = nome
